9e472c4ec0ccf3b91cd0136be51b7a1fb4101862
Joseph Gruber <590669+josephgruber@users.noreply.github.com>
2025-10-05 10:37:30 -0600

    Add direnv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
 ln -s ~/.dotfiles/.profile ~/.profile
 ln -s ~/.dotfiles/git/.zprofile ~/.zprofile
 
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
+
+ln -s ~/.dotfiles/ssh/config ~/.ssh/config
